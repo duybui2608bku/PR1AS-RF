915333c96d9be271968f818cc3594463fc57cea3
TuanAnhDevIT <dangtrantuananh.2002@gmail.com>
2026-04-09 23:34:58 +0700

    feat: add design system documentation inspired by Airbnb, including visual themes, color palette, typography rules, and component stylings

--- CLIENT/eslint.config.mjs
+++ CLIENT/eslint.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Custom rules for the client.
   {
+    plugins: {
+      prettier: prettierPlugin,
+    },
     rules: {
       "linebreak-style": "off",
-      "prettier/prettier": ["error", { endOfLine: "crlf" }],
+      "prettier/prettier": ["warn", { endOfLine: "auto" }],
     },
   },
   // Override default ignores of eslint-config-next.
